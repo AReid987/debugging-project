24e15bd215ac8305161475be237888921fe06245
Alice <alicekb01@gmail.com>
2018-04-10 01:56:09 -0400

    Add error to component

--- src/components/ImgComponent.jsx
+++ src/components/ImgComponent.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const ImgComponent = ({ images, type }) => {
-  const parsedImages = images.map((url, index) => { return <img src={url}   alt={type} key={index} className={'display-img'}/>
+  const parsedImages = images.map((url, index) => { 
+    <img src={url} alt={type} key={index} className={'display-img'}/>
   })
-
+  
+  console.warn = '' //Do not Edit! Hides the error messages just for this example!
   return (
     <React.Fragment>
       { parsedImages }
